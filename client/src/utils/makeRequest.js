f96607069f305c94ba0bd8dd14327b822e14e129
Numanur <nomanur016@gmail.com>
2025-07-18 23:29:58 +0600

    update

--- client/src/utils/makeRequest.js
+++ client/src/utils/makeRequest.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 // const REACT_APP_API_URL = 'http://localhost:5000/api';
 // const REACT_APP_API_URL = 'https://blockchain-project-api.vercel.app/api';
-const REACT_APP_API_URL = 'https://blockchain-api-pt82.onrender.com/api';
+const REACT_APP_API_URL = "https://trustbazar-backend.onrender.com";
 
 export const publicRequest = axios.create({
-    baseURL: REACT_APP_API_URL
+  baseURL: REACT_APP_API_URL,
 });
-
