dede183e066a6c54c67132188ea1c0217cece377
Numanur <116955339+Numanur@users.noreply.github.com>
2025-07-18 23:32:57 +0600

    Delete api directory

--- api/README.md
+++
@@ -1,2 +0,0 @@
-# blockchain-project-api
-Backend for Render Deploy
--- api/config/dbConn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("MongoDB connection successful"))
-    .catch(err => console.error(err));
