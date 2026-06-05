c1ae1896b9d28d055585bc0fdf6829b59ec068a7
Numanur <nomanur016@gmail.com>
2026-06-06 02:51:33 +0600

    change api url

--- client/src/utils/makeRequest.js
+++ client/src/utils/makeRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const REACT_APP_API_URL = "http://localhost:5000/api";
-// const REACT_APP_API_URL = "https://trustbazar-backend.onrender.com/api";
+// const REACT_APP_API_URL = "http://localhost:5000/api";
+const REACT_APP_API_URL = "https://trustbazar-backend.onrender.com/api";
 
 export const publicRequest = axios.create({
   baseURL: REACT_APP_API_URL,
