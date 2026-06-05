19162473a63df677a4bc61f6265b338c47955584
Md. Numanur Rahman <116955339+Numanur@users.noreply.github.com>
2026-06-05 23:00:15 +0600

    Change Firebase configuration
    Updated Firebase configuration with new project details.

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -4,13 +4,22 @@ const FIREBASE_API_KEY = "AIzaSyD4kwpr4MXiPSPnon_LI4scI_Gfn7dkgJA";
 const FIREBASE_APP_ID = "1:378539980411:web:c6f7494f82828bbf63d4de";
 
 // Web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: FIREBASE_API_KEY,
+//     authDomain: "zenshop-a95d9.firebaseapp.com",
+//     projectId: "zenshop-a95d9",
+//     storageBucket: "zenshop-a95d9.appspot.com",
+//     messagingSenderId: "378539980411",
+//     appId: FIREBASE_APP_ID
+// };
+
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: "zenshop-a95d9.firebaseapp.com",
-    projectId: "zenshop-a95d9",
-    storageBucket: "zenshop-a95d9.appspot.com",
-    messagingSenderId: "378539980411",
-    appId: FIREBASE_APP_ID
+  apiKey: "AIzaSyCDl9RNxau1wf4BdTTmZrko41mcKX_inJY",
+  authDomain: "trustbazar.firebaseapp.com",
+  projectId: "trustbazar",
+  storageBucket: "trustbazar.firebasestorage.app",
+  messagingSenderId: "95438939228",
+  appId: "1:95438939228:web:e1d5492cfda3035dbab299",
 };
 
 // Initialize Firebase
